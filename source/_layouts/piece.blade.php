e57937d7b2ad9de57b9383a8a2b9489c65c9e0f2
Colin Fitz-Maurice <colin@fitz-maurice.com>
2018-01-04 00:10:37 -0500

    fix single piece layout

--- source/_layouts/piece.blade.php
+++ source/_layouts/piece.blade.php
@@ -2,11 +2,11 @@
 
 @section('body')
 
-<div class="flex flex-col sm:flex-row">
-    <div class="w-2/3">
+<div class="flex flex-col md:flex-row">
+    <div class="w-full md:w-2/3">
         <img class="shadow hover:shadow-lg w-full" src="{{ $page->img }}">
     </div>
-    <div class="w-1/3 px-4">
+    <div class="w-full md:w-1/3 md:px-4 mt-4 md:mt-0">
         <h1 class="border-b-2 border-yellow pb-2 mb-4">{{ $page->title }}</h1>
 
         @yield('content')
