2f1b54527afb43a276d2180964b90b5119a1632d
Colin Fitz-Maurice <colin@fitz-maurice.com>
2018-01-07 14:31:52 -0500

    Images and sub page layout

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,12 +5,12 @@
 <div class="flex flex-col sm:flex-row">
     <div class="w-full sm:w-1/2 px-0 sm:pr-4 my-4">
         <a href="">
-            <img class="shadow hover:shadow-lg w-full rounded" src="/images/acme/acme_products.png">
+            <img class="shadow hover:shadow-lg w-full" src="/images/acme/acme_products.png">
         </a>
     </div>
     <div class="w-full sm:w-1/2 px-0 sm:pl-4 my-4">
         <a href="">
-            <img class="shadow hover:shadow-lg w-full rounded" src="/images/acme/acme_products.png">
+            <img class="shadow hover:shadow-lg w-full" src="/images/acme/acme_products.png">
         </a>
     </div>
 </div>
@@ -18,17 +18,17 @@
 <div class="flex flex-col sm:flex-row mb-4">
     <div class="w-full sm:w-1/3 px-0 sm:pr-4 my-4">
         <a href="#">
-            <img class="shadow hover:shadow-lg w-full rounded" src="/images/acme/acme_products.png">
+            <img class="shadow hover:shadow-lg w-full" src="/images/acme/acme_products.png">
         </a>
     </div>
     <div class="w-full sm:w-1/3 px-0 sm:px-2 my-4">
         <a href="#">
-            <img class="shadow hover:shadow-lg w-full rounded" src="/images/acme/acme_products.png">
+            <img class="shadow hover:shadow-lg w-full" src="/images/acme/acme_products.png">
         </a>
     </div>
     <div class="w-full sm:w-1/3 px-0 sm:pl-4 my-4">
         <a href="#">
-            <img class="shadow hover:shadow-lg w-full rounded" src="/images/acme/acme_products.png">
+            <img class="shadow hover:shadow-lg w-full" src="/images/acme/acme_products.png">
         </a>
     </div>
 </div>
--- source/stylistic/index.blade.php
+++ source/stylistic/index.blade.php
@@ -7,41 +7,42 @@
     <div class="flex flex-col md:flex-row justify-center mt-4">
 
         <div class="flex flex-row md:flex-col flex-wrap items-center justify-between w-full md:w-1/5">
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full pr-1 md:pr-0">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full mr-1 md:mr-0">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full mr-1 md:mr-0">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
         </div>
 
-        <div class="flex-grow min-w-screen px-4">
+        <div class="flex-grow min-w-screen my-4 md:my-0 md:px-4">
             <lightbox inline-template image="/images/acme/acme_thumb.png">
                 <div>
-                    <img class="shadow hover:shadow-lg w-full rounded cursor-pointer" src="/images/acme/acme_thumb.png" @click="index = 0">
+                    <img class="shadow hover:shadow-lg w-full cursor-pointer" src="/images/acme/acme_thumb.png" @click="index = 0">
                     <gallery :images="images" :index="index" @close="index = null"></gallery>
                 </div>
             </lightbox>
+
         </div>
 
         <div class="flex flex-row md:flex-col flex-wrap items-center justify-between w-full md:w-1/5">
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full mr-1 md:mr-0">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full mr-1 md:mr-0">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
-            <a href="#" class="shadow hover:shadow-lg rounded border-4 border-black mb-2 w-1/2 md:w-full">
-                <img class="w-full rounded" src="/images/acme/acme_thumb.png">
+            <a href="#" class="shadow hover:shadow-lg border-2 border-black mb-2 w-49 md:w-full">
+                <img class="w-full" src="/images/acme/acme_thumb.png">
             </a>
         </div>
     </div>
