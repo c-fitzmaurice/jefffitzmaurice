c2ce30929a14011a0cfe4989317b8c1fb074bac4
Colin <colin@fitz-maurice.com>
2018-04-25 00:59:04 -0400

    Homepage rows

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,14 +2,16 @@
 
 @section('body')
 
-<div class="flex flex-col sm:flex-row mb-4">
-    @foreach($pieces as $piece)
-        <div class="w-full sm:w-1/3 px-0 sm:pr-4 my-4">
-            <a href="#">
-                <img class="shadow hover:shadow-lg w-full" src="{{ $piece->thumb($piece->img) }}" alt="{{ $piece->title }}">
-            </a>
-        </div>
-    @endforeach
-</div>
+@foreach ($pieces->chunk(3) as $_pieces)
+    <div class="flex flex-col sm:flex-row mb-1">
+        @foreach($_pieces as $piece)
+            <div class="w-full sm:w-1/3 px-0 sm:pr-4 mt-4">
+                <a href="#">
+                    <img class="shadow hover:shadow-lg w-full" src="{{ $piece->thumb($piece->img) }}" alt="{{ $piece->title }}">
+                </a>
+            </div>
+        @endforeach
+    </div>
+@endforeach
 
 @endsection
