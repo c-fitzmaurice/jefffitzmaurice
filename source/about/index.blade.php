fcfc9b99ad42bd3923b8d593d27b3302ddda3795
Colin <colin@fitz-maurice.com>
2018-05-01 23:02:31 -0400

    About page updates

--- source/about/index.blade.php
+++ source/about/index.blade.php
@@ -2,13 +2,16 @@
 
 @section('body')
 
-    <div class="text-center">
-        <h1 class="uppercase text-md font-sans text-yellow">About</h1>
+    <h1 class="uppercase text-center text-md font-sans text-yellow">Jeff Fitz-Maurice</h1>
 
-        <div class="text-left">
-            Hello world
+    <article class="flex mt-8">
+        <div class="w-1/2 pr-4">
+            <p class="mb-4 leading-loose">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p class="mb-4 leading-loose">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis.</p>
         </div>
-    </div>
-
+        <div class="w-1/2">
+            <img src="/images/washington-post-sunday/washington-post-sunday.jpg" alt="">
+        </div>
+    </article>
 
 @endsection
