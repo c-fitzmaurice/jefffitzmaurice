04f665d3c55a38eedef54abfc2e6d06bdfc7edd0
Colin <colin@fitz-maurice.com>
2018-06-13 01:13:45 -0400

    working on the homepage

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,9 +4,15 @@
 
 <div class="flex flex-wrap -mx-2">
     @foreach($pieces as $piece)
-        <a class="inline-block w-1/2 md:w-1/3 px-2 mt-4" href="{{ $piece->link($piece->img) }}">
+        {{-- <a class="inline-block w-1/2 md:w-1/3 px-2 mt-4" href="{{ $piece->link($piece->img) }}">
             <img class="shadow hover:shadow-lg min-w-full min-h-full" src="{{ $piece->thumb($piece->img) }}" alt="{{ $piece->title }}">
-        </a>
+        </a> --}}
+        <lightbox inline-template image="{{ $piece->img }}">
+            <div class="inline-block w-1/2 md:w-1/3 px-2 mt-4">
+                <img class="shadow hover:shadow-lg min-w-full min-h-full" @click="index = 0" src="{{ $piece->thumb($piece->img) }}" alt="{{ $piece->title }}">
+                <gallery :images="images" :index="index" @close="index = null"></gallery>
+            </div>
+        </lightbox>
     @endforeach
 </div>
 
