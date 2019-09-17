c07d382f6060fc70a2f8f7f19f0c09cb1e8220b7
Colin Fitz-Maurice <colin.fitzmaurice@skookum.com>
2019-09-17 08:55:06 -0700

    footer link

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -4,7 +4,7 @@
             <p class="text-sm">&copy; {{ date('Y') }} {{ $page->site_name }}</p>
         </div>
         <div class="w-full sm:w-1/2 text-center sm:text-right py-1">
-            <p class="text-sm">Website by <a class="border-solid border-b-1 border-yellow" href="https://fitz-maurice.com" target="_blank">Fitz Creative</a></p>
+            <p class="text-sm">Website by <a class="border-solid border-b-1 border-yellow" href="https://fitz-maurice.com" target="_blank" rel="noreferrer">Fitz Creative</a></p>
         </div>
     </div>
 </footer>
