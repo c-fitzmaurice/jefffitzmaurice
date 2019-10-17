04022c106e3b988c2a818687d63e22156211061a
Colin <colin@fitz-maurice.com>
2019-10-16 22:43:37 -0400

    edit bio

--- source/about/index.blade.php
+++ source/about/index.blade.php
@@ -6,7 +6,7 @@
     <article class="flex flex-col flex-col-reverse md:flex-row mt-8">
         <div class="w-full md:w-1/2 pr-4">
             <p class="mb-4 leading-loose text-lg">It has been a real pleasure and an honor over the years having had the privilege of creating illustrations for many of the highest profile companies, advertising agencies and publishers in the country.</p>
-            <p class="mb-4 leading-loose text-lg">That experience has also given me the enjoyable opportunity to work for and collaborate with many very talented art directors and designers to whom I owe thanks and appreciation for the opportunity and trust.</p>
+            <p class="mb-4 leading-loose text-lg">That experience has also given me the enjoyable opportunity to work for and collaborate with many very talented art directors and designers to whom I owe thanks and appreciation.</p>
             <p class="mb-4 leading-loose text-lg">A partial list of the companies and publishers I’ve worked for:</p>
             <p class="mb-4 leading-loose text-lg">The National Football League, Celestial Seasoning’s, AT&T, Merck, Bristol-Myers Squibb, Alka Seltzer, Acme Foods, Turkey Hill, Campbell’s, Edy’s Ice Cream, McDonalds, Coke, Seagram’s, Random House Books, Bantam Books, Penguin Books, Pocket Books, Berkley Publishing, The Washington Post</p>
         </div>
