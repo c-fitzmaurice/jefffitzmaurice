968c598f2920a9a82f20f2bb958e9361b00126b1
Colin <colin@fitz-maurice.com>
2019-10-16 20:50:39 -0400

    update vue-gallery

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,10 +5,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="{{ $page->meta_description }}">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap|Playfair+Display">
         <link rel="stylesheet" href="/css/main.css">
-        <meta name="description" content="{{ $page->meta_description }}">
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
     </head>
     <body class="bg-white">
         <div id="jeff" class="border-solid border-t-4 border-black flex flex-col min-h-screen">
