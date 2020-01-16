08b16a34df4930f00d5c022b4afa4597e93a4fd3
Colin <colin@fitz-maurice.com>
2020-01-16 02:11:07 -0500

    package updates and jigsaw build files

--- tasks/bin.js
+++ tasks/bin.js
@@ -5,15 +5,17 @@ var path = require('path');
 module.exports = {
     path: function() {
         if (fs.existsSync('./vendor/bin/jigsaw')) {
-            return path.normalize('./vendor/bin/jigsaw')
+            return path.normalize('./vendor/bin/jigsaw');
         }
 
         if (hasbin.sync('jigsaw')) {
             return 'jigsaw';
         }
 
-        console.log('Could not find Jigsaw; please install it via Composer, either locally or globally.');
+        console.log(
+            'Could not find Jigsaw; please install it via Composer, either locally or globally.'
+        );
 
         process.exit();
-    }
+    },
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,52 +1,33 @@
-const argv = require('yargs').argv;
-const command = require('node-cmd');
-const mix = require('laravel-mix');
-const OnBuild = require('on-build-webpack');
-const Watch = require('webpack-watch');
-const bin = require('./tasks/bin');
-const env = argv.e || argv.env || 'local';
-
+let mix = require('laravel-mix');
+let build = require('./tasks/build.js');
 require('laravel-mix-purgecss');
 require('laravel-mix-tailwind');
 
-// Every build
-const plugins = [
-    new OnBuild(() => {
-        command.get(bin.path() + ' build ' + env, (error, stdout, stderr) => {
-            if (error) {
-                console.log(stderr);
-                process.exit(1);
-            }
-            console.log(stdout);
-        });
-    }),
-    new Watch({
-        paths: ['source/**/*.md', 'source/**/*.php'],
-        options: {
-            ignoreInitial: true
-        }
-    })
-];
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+mix.webpackConfig({
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch([
+            'source/**/*.md',
+            'source/**/*.php',
+            'source/**/*.scss',
+            '!source/**/_tmp/*',
+        ]),
+    ],
+});
 
-mix
-.webpackConfig({ plugins })
-.setPublicPath('source')
-.browserSync({
-    proxy: 'https://jefffitzmaurice.test/',
-    files: [
-        './build_local/**/*.html',
-        './build_local/**/*.js',
-        './build_local/**/*.css'
-    ]
-})
-.sass('source/_assets/sass/main.scss', 'source/css')
-    .tailwind()
+mix.js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css')
+    .tailwind('./tailwind.config.js')
     .purgeCss({
         globs: [
             path.join(__dirname, 'source/**/*.blade.php'),
-            path.join(__dirname, 'source/_assets/**/*.blade.php')
-        ]
+            path.join(__dirname, 'source/_assets/**/*.blade.php'),
+        ],
     })
-    .version()
-.js('source/_assets/js/main.js', 'source/js')
+    .options({
+        processCssUrls: false,
+    })
     .version();
